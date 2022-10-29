79c635ad4c10402893ff3c27d4d241bcacf5de77
GexCodexTogether <getcodetogether@gmail.com>
2022-10-29 10:03:51 +0200

    feat(motor.py): added example motor

---
+++ Linienfolger/motor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+
+# TODO: Add code here
