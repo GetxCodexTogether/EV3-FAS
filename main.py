95d077990f82b12b2accf02f7c4019a34506b178
Simon <getcodetogether@gmail.com>
2022-11-17 20:38:20 +0100

    merge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+<<<<<<< HEAD
 from ev3dev2.motor import LargeMotor
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM
 from time import sleep
@@ -42,3 +43,53 @@ sleep(1)
 
 # 100 rotations per minute(RPM)
 lm.on_for_seconds(speed=SpeedRPM(100), seconds=3)
+=======
+from time import sleep
+#from ev3dev2.button import Button
+
+from linefollower.linefollower import *
+#from linefollower.sensor import *
+#from matplotlib.pyplot import mt
+#import matplotlib.pylab as plt 
+#import numpy as np
+
+#motor_init()
+#sensor_init()
+
+# import matplotlib.pylab as plt 
+# import math
+# import numpy as np
+
+# print("Hello World1")
+
+# xpoints = np.array([1, 8])
+# ypoints = np.array([3, 10])
+
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+#import logging
+
+
+# config logging
+#logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+#logging.debug('Run robot, run!')
+
+from ev3dev2.sound import Sound
+
+spkr = Sound()
+
+# Play 'bark.wav':
+
+
+# Introduce yourself:
+#spkr.set_volume(255)
+#spkr.speak('Hallo, ich heisse Roberto Ev und wurde von Simon und Laura programmiert')
+#spkr.speak('Hello, i am Roberto Ev and i was programmed by Simon and Laura')
+
+print("Main-Loop")
+while 1:
+    liniep_controller()
+    
+>>>>>>> 727373bf98529acd799c9c61b8cda09fc25f69a3
