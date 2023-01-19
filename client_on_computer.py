dcad2afae29854dc6751be224cf430d5573d0f0c
Simon <getcodetogether@gmail.com>
2023-01-19 22:18:23 +0100

    noplot

--- client_on_computer.py
+++ client_on_computer.py
@@ -19,13 +19,10 @@ IPaddress = '192.168.178.45'
 s = socket.socket()
 s.connect((IPaddress, 12345))
 
-plot.style.use('fivethirtyeight')
-fig = plot.figure(num=None, figsize=[10, 7])
-ax = fig.add_subplot(111)
 rcvdData = 'None'
-x = []
-y = []
-def animate(i):
+
+
+def test():
     rcvdData = s.recv(1024).decode()
     if rcvdData != 'end':
         print(rcvdData)
@@ -35,13 +32,6 @@ def animate(i):
         rcvdDatalist[2] = float(rcvdDatalist[2])
         rcvdDatalist[3] = float(rcvdDatalist[3])
 
-        x.append(rcvdDatalist[0])
-        
-        y.append(rcvdDatalist[1])
-        plot.cla()
-        plot.plot(x, y)
-
-        
         with open('plotdata.csv', 'a',newline='') as csvfile:
             filewriter = csv.writer(csvfile, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
             filewriter.writerow([float(rcvdDatalist[0]), float(rcvdDatalist[1]),float(rcvdDatalist[2]),float(rcvdDatalist[3]) ])
@@ -49,23 +39,9 @@ def animate(i):
         # Back to Server
         sendData = 'received data!'
         s.send(sendData.encode())
-        
-        
-        #PLOT
-        plot.scatter(rcvdDatalist[0], rcvdDatalist[1], s = 500, color='green', marker="X")
-        plot.xlabel('Time (seconds)')
-        plot.ylabel('Sensor-Wert')
-        ax.set_xlim(left=rcvdDatalist[0]-15, right=rcvdDatalist[0]+15)
-        ax.set_ylim(bottom=300, top=700)
-    else:
-        sendData = 'ending process...'
-        s.send(sendData.encode())
-        s.close()
-        sys.exit('Connection ended!')
 
-
-ani = FuncAnimation(plot.gcf(), animate, interval=10)
-plot.show()
+while(1):
+    test()
 
 
 
